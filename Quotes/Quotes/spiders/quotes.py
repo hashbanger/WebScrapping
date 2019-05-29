af995810bfcd40ad4fe418e7f98ae622ce833f27
hashbanger <prashant.brahmbhatt32@outlook.com>
2019-05-29 13:01:50 +0530

    added the final spider

--- Quotes/Quotes/spiders/quotes.py
+++ Quotes/Quotes/spiders/quotes.py
@@ -4,11 +4,23 @@ import scrapy
 
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
-    allowed_domains = ['quotes.toscrape.com/']
-    start_urls = ['http://quotes.toscrape.com//']
+    allowed_domains = ['quotes.toscrape.com']
+    start_urls = ['http://quotes.toscrape.com/']
 
     def parse(self, response):
-        header_1 = response.xpath('//h1/a/text()').extract()
-        tag_list = response.xpath('//*[@class = "tag-item"]/a/text()').extract()
-        
-        yield {'h1': header_1, 'tags': tag_list}
+        quotes = response.xpath('//*[@class = "quote"]')
+        # iterating over each individual quote class
+        for quote in quotes:
+            text = quote.xpath('.//*[@class = "text"]/text()').extract_first()
+            author = quote.xpath('.//*[@class = "author"]/text()').extract_first()
+            tags = quote.xpath('.//*[@itemprop = "keywords"]/@content').extract_first()
+
+            yield{'Text': text,
+                  'Author': author,
+                  'Tags': tags}
+              
+            # Now we append the path of the next page
+
+        next_page_url = response.xpath('//*[@class = "next"]/a/@href').extract_first()
+        absolute_next_page_url = response.urljoin(next_page_url)
+        yield scrapy.Request(absolute_next_page_url) # outside the parse function the callback will also be needed
